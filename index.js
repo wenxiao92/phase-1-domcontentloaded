99df9a177eeec72c215dee5cbd0c440db0f38a3b
Wen Xiao <wennstyle@gmail.com>
2021-10-10 18:48:25 -0400

    commit tree b1f8ac91c77d84b4a4298d8689d34b2370923209

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    const variable = document.getElementById("text")
+    variable.textContent = "This is really cool!"
+})
